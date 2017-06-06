9152e7e128be8ca3e60bd62767e2eec4338d4c0c
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-06 16:20:27 -0300

    Clean file

--- app/pages/assets.js
+++ app/pages/assets.js
@@ -1,36 +1,17 @@
-/* Based on the template in Web Starter Kit :
-https://github.com/google/web-starter-kit/blob/master/app/index.html
-*/
-
 const metaAssets = () => {
   return [
     { charset: 'utf-8' },
-    // Meta descriptions are commonly used on search engine result pages to
-    // display preview snippets for a given page.
     { name: 'description', content: 'Your One-Stop solution for a full-stack universal Redux App' },
-    // Setting IE=edge tells Internet Explorer to use the latest engine to
-    //  render the page and execute Javascript
     { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-    // Using the viewport tag allows you to control the width and scaling of
-    // the browser's viewport:
-    // - include width=device-width to match the screen's width in
-    // device-independent pixels
-    // - include initial-scale=1 to establish 1:1 relationship between css pixels
-    // and device-independent pixels
-    // - ensure your page is accessible by not disabling user scaling.
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    // Disable tap highlight on IE
     { name: 'msapplication-tap-highlight', content: 'no' },
-    // Add to homescreen for Chrome on Android
     { name: 'mobile-web-app-capable', content: 'yes' },
-    // Add to homescreen for Safari on IOS
     { name: 'apple-mobile-web-app-capable', content: 'yes' },
     { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
     { name: 'apple-mobile-web-app-title', content: 'reactGo' },
-    // Tile icon for Win8 (144x144 + tile color)
     { name: 'msapplication-TileColor', content: '#3372DF' }
   ];
 };
 
-export const title = 'reactGo - just ship it';
+export const title = 'Travel planner';
 export const meta = metaAssets();
