eef8233f28c38c507097b32e3a51e4ac820fab90
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-07 01:08:12 -0300

    First edit travel components

--- app/components/TravelItem.jsx
+++ app/components/TravelItem.jsx
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const TravelItem = ({ id, destination, startDate, endDate, comments, deleteTravel }) => {
-  const onDelete = () => {
-    return function() {
-      deleteTravel(id);
+export default class TravelItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { destination: this.props.destination,
+      comments: this.props.comments,
+      startDate: this.props.startDate,
+      endDate: this.props.endDate
     }
   }
 
-  return (
-    <div>
-      { destination }
-      { startDate }
-      { endDate }
-      { comments }
-      <button onClick={ onDelete() }>
-        Delete travel
-      </button>
-    </div>
-  );
+  updateDestination(event) {
+    this.setState({ destination: event.target.value });
+  }
+
+  updateComments(event) {
+    this.setState({ comments: event.target.value });
+  }
+
+  updateStartDate(event) {
+    this.setState({ startDate: event.target.value });
+  }
+
+  updateEndDate(event) {
+    this.setState({ endDate: event.target.value });
+  }
+
+  onDelete() {
+    this.props.deleteTravel(id);
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.destination }
+        { this.state.startDate }
+        { this.state.endDate }
+        { this.state.comments }
+        <br />
+        <button onClick={ this.onDelete.bind(this) }>
+          Delete travel
+        </button>
+        <input
+          placeholder="Travel destination"
+          onChange={this.updateDestination.bind(this)}
+          value={this.state.destination} />
+        <input
+          placeholder="Travel comments"
+          onChange={this.updateComments.bind(this)}
+          value={this.state.comments} />
+        <input
+          type="date"
+          onChange={this.updateStartDate.bind(this)}
+          value={this.state.startDate} />
+        <input
+          type="date"
+          onChange={this.updateEndDate.bind(this)}
+          value={this.state.endDate} />
+        <button>
+          Edit Travel
+        </button>
+      </div>
+    );
+  }
 };
 
 TravelItem.propTypes = {
@@ -29,5 +74,3 @@ TravelItem.propTypes = {
   comments: PropTypes.string.isRequired,
   deleteTravel: PropTypes.func.isRequired
 };
-
-export default TravelItem;
