74569f2d9f0d1345387a6abec8b5751d2ea8fe5b
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-07 00:53:12 -0300

    Remove comments

--- server/index.js
+++ server/index.js
@@ -9,20 +9,10 @@ import renderMiddleware from './render/middleware';
 
 const app = express();
 
-/*
- * Database-specific setup
- * - connect to MongoDB using mongoose
- * - register mongoose Schema
- */
 connect();
-
-/*
- * REMOVE if you do not need passport configuration
- */
 initPassport();
 
 if (isDebug) {
-  // enable webpack hot module replacement
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const webpackConfig = require('../webpack/webpack.config');
@@ -32,24 +22,8 @@ if (isDebug) {
   app.use(webpackHotMiddleware(compiler));
 }
 
-/*
- * Bootstrap application settings
- */
 initExpress(app);
-
-/*
- * REMOVE if you do not need any routes
- *
- * Note: Some of these routes have passport and database model dependencies
- */
 initRoutes(app);
 
-/*
- * This is where the magic happens. We take the locals data we have already
- * fetched and seed our stores with data.
- * renderMiddleware matches the URL with react-router and renders the app into
- * HTML
- */
 app.get('*', renderMiddleware);
-
 app.listen(app.get('port'));
