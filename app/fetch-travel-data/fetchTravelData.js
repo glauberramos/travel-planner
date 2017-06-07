84d8b003c83bcbcd660b762aafd7cc9de27c2627
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-07 02:10:56 -0300

    Remove comments

--- app/fetch-travel-data/fetchTravelData.js
+++ app/fetch-travel-data/fetchTravelData.js
@@ -3,8 +3,6 @@ import { travelService } from '../services';
 const fetchTravelData = () => {
   return travelService().getTravels()
   .then(res => res.data)
-  // Returning [] as a placeholder now so it does not error out when this service
-  // fails. We should be handling this in our DISPATCH_REQUEST_FAILURE
   .catch(() => []);
 };
 
