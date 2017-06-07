49f03d1dc46f0c25c83a40940d5ea88e49b5d660
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-07 18:19:23 -0300

    Fields for editing user and deleting it

--- app/components/UserItem.jsx
+++ app/components/UserItem.jsx
@@ -6,8 +6,33 @@ export default class UserItem extends Component {
     super(props);
     this.state = {
       email: this.props.email,
-      role: this.props.role
+      role: this.props.role,
+      password: ''
     }
+  }
+
+  updateEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  updateRole(event) {
+    this.setState({ role: event.target.value });
+  }
+
+  updatePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  onDelete() {
+    // this.props.Travel(this.props.id);
+  }
+
+  onUpdate() {
+    // this.props.updateTravel(this.props.id,
+    //   this.state.destination,
+    //   this.state.comments,
+    //   this.state.startDate,
+    //   this.state.endDate);
   }
 
   render() {
@@ -16,6 +41,25 @@ export default class UserItem extends Component {
         { this.props.id }
         { this.state.email }
         { this.state.role }
+        <br />
+        <button onClick={ this.onDelete.bind(this) }>
+          Delete user
+        </button>
+        <input
+          placeholder="email"
+          onChange={this.updateEmail.bind(this)}
+          value={this.state.email} />
+        <input
+          placeholder="role"
+          onChange={this.updateRole.bind(this)}
+          value={this.state.role} />
+        <input
+          type="password"
+          onChange={this.updatePassword.bind(this)}
+          value={this.state.password} />
+        <button onClick={ this.onUpdate.bind(this) }>
+          Edit User
+        </button>
       </div>
     );
   }
