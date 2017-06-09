41cf2ee4267eb74abcfe9de8430460cbfca7c050
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-09 01:49:21 -0300

    Styling create trip box

--- app/components/travel/TravelCreation.jsx
+++ app/components/travel/TravelCreation.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Travel.css';
+const cx = classNames.bind(styles);
 
 const ENTER_KEY_CODE = 13;
 
@@ -31,25 +34,31 @@ export default class TravelCreation extends Component {
 
   render() {
     return (
-      <div>
-        <h1>What's your next destination?</h1>
+      <div className={cx('create-box')}>
+        <h1 className={cx('header')}>Plan your next trip!</h1>
         <input
-          placeholder="Trip destination"
+          className={cx('input-create')}
+          placeholder="Destination"
           onChange={this.updateDestination.bind(this)}
           value={this.state.destination} />
         <input
-          placeholder="Trip comments"
+          className={cx('input-create')}
+          placeholder="Comments"
           onChange={this.updateComments.bind(this)}
           value={this.state.comments} />
         <input
+          className={cx('input-create')}
           type="date"
           onChange={this.updateStartDate.bind(this)}
           value={this.state.startDate} />
         <input
+          className={cx('input-create')}
           type="date"
           onChange={this.updateEndDate.bind(this)}
           value={this.state.endDate} />
-        <button onClick={this.onSave.bind(this)}>
+        <button
+          className={cx('button', 'primary', 'no-margin')}
+          onClick={this.onSave.bind(this)}>
           Create Trip
         </button>
       </div>
