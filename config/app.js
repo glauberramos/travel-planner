7878e3621aaaa4bb7f1ca7b0b2523bace7a5a51e
Glauber dutra ramos <glauberamos@gmail.com>
2017-06-12 20:37:53 -0300

    Change heroku url

--- config/app.js
+++ config/app.js
@@ -4,7 +4,6 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://demo-reactgo.herokuapp.com';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://travel-toptal.herokuapp.com';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
-
